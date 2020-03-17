f85727f6165cf9f798ad927bf2651284374a2983
andre-sav <49045240+andre-sav@users.noreply.github.com>
2020-03-17 14:04:12 -0700

    Add files via upload

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,28 +1,35 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        min_index = i 
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i],arr[min_index] = arr[min_index],arr[i]
 
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+# # TO-DO:  implement the Bubble Sort function below
+# def bubble_sort( arr ):
+#     # for iterable in ~100
+#     for i in range(len(arr)):
+#         # for iterable2 in 100-iterable-1
+#         for j in range(0,len(arr)-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
+#     return arr
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(i+1, n):
+            if arr[i]>arr[j]:
+                arr[i],arr[j] = arr[j],arr[i]
     return arr
-
+        
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
